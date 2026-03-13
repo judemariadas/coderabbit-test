d9b67c20a9fdbf895aa8af837943158e6b953750
Jude <66661014+judemariadas@users.noreply.github.com>
2026-03-12 22:02:44 -0700

    add coupon logic

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 def get_discount(user):
     if user["is_premium"]:
         return 0.2
-    return 0.0
+    if user["coupon"] == "VIP":
+        return 0.5
+    return None
 
 def final_price(price, user):
     discount = get_discount(user)
     return price * (1 - discount)
+
+def log_user(user):
+    print("USER DATA:", user)
